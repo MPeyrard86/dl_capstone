4725b330183b6b716a74e4aadb93e7fd264220d4
Matthew Peyrard <matt.peyrard@gmail.com>
2016-10-17 20:19:00 -0400

    Bigger network, performance still awful. Checkpoint before I blow it up and try again.

--- utils/train/convnet_trainer.py
+++ utils/train/convnet_trainer.py
@@ -24,6 +24,8 @@ def show_usage():
     print('Usage: python convnet_trainer.py <input CSV> <output-folder>')
 
 def weight_variable(name, shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+def conv_weight_variable(name, shape):
     return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer_conv2d())
 def bias_variable(name, shape):
     return tf.Variable(tf.constant(1.0, shape=shape), name=name)
@@ -33,11 +35,11 @@ def max_pool(X):
     return tf.nn.max_pool(X, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 def input_conv_layer(name, input, kernel_size, input_channels, output_channels):
-    W_conv = weight_variable("W_" + name, [kernel_size, kernel_size, input_channels, output_channels])
+    W_conv = conv_weight_variable("W_" + name, [kernel_size, kernel_size, input_channels, output_channels])
     b_conv = bias_variable("b_" + name, [output_channels])
     return tf.nn.relu(conv2d(input, W_conv) + b_conv)
 def conv_layer(name, input, kernel_size, input_channels, output_channels, drop_prob, pool=False):
-    W_conv = weight_variable("W_" + name, [kernel_size, kernel_size, input_channels, output_channels])
+    W_conv = conv_weight_variable("W_" + name, [kernel_size, kernel_size, input_channels, output_channels])
     b_conv = bias_variable("b_" + name, [output_channels])
     layer = tf.nn.relu(conv2d(input, W_conv) + b_conv)
     return tf.nn.dropout(max_pool(layer) if pool else layer, drop_prob)
@@ -109,14 +111,23 @@ for training_sample in training_set:
     # Record normalized training samples.
     normalized_training_set.append((training_image, length_label, digit_labels))
 
+#Temporary for debugging
+normalized_training_set = normalized_training_set[0:1000]
+
+
 
 conv_kernel_size = 3
 conv1_depth = 48
 conv2_depth = 64
 conv3_depth = 128
+conv4_depth = 160
+conv5_depth = 192
+conv6_depth = 192
+conv7_depth = 192
+conv8_depth = 192
 fc1_features = 4096
 
-training_batch_size = 64
+training_batch_size = 8
 training_iterations = 20000
 
 graph = tf.Graph()
@@ -128,9 +139,15 @@ with graph.as_default():
     keep_probability = tf.placeholder(tf.float32)
 
     conv1 = input_conv_layer("conv1", X, conv_kernel_size, num_colour_channels, conv1_depth)
-    conv2 = conv_layer("conv2", conv1, conv_kernel_size, conv1_depth, conv2_depth, keep_probability, pool=True)
-    conv3 = conv_layer("conv3", conv2, conv_kernel_size, conv2_depth, conv3_depth, keep_probability, pool=True)
-    flat, nf = flatten_layer(conv3)
+    conv2 = conv_layer("conv2", conv1, conv_kernel_size, conv1_depth, conv2_depth, keep_probability)
+    conv3 = conv_layer("conv3", conv2, conv_kernel_size, conv2_depth, conv3_depth, keep_probability)
+    conv4 = conv_layer("conv4", conv3, conv_kernel_size, conv3_depth, conv4_depth, keep_probability, pool=True)
+    conv5 = conv_layer("conv5", conv4, conv_kernel_size, conv4_depth, conv5_depth, keep_probability)
+    conv6 = conv_layer("conv6", conv5, conv_kernel_size, conv5_depth, conv6_depth, keep_probability)
+    conv7 = conv_layer("conv7", conv6, conv_kernel_size, conv6_depth, conv7_depth, keep_probability, pool=True)
+    conv8 = conv_layer("conv8", conv7, conv_kernel_size, conv7_depth, conv8_depth, keep_probability)
+
+    flat, nf = flatten_layer(conv8)
     fc1 = dense_layer("fc1", flat, nf, fc1_features, keep_probability)
     digit_length = output_layer("output", fc1, fc1_features, num_length_classes)
 
@@ -139,7 +156,6 @@ with graph.as_default():
 
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=digit_length, labels=y_length)
     loss_function = tf.reduce_mean(cross_entropy)
-    optimizer = tf.train
 
 
     # loss = tf.nn.softmax_cross_entropy_with_logits(logits=digit_length, labels=y_length)
@@ -168,109 +184,3 @@ with tf.Session(graph=graph) as session:
             print("iteration {0:>6}, accuracy: {1:>6.1%}".format(i + 1, acc))
             # print('Minibatch loss at step %d: %f' % (i, l))
             # print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch[1]))
-
-# conv1_num_filters = 48
-# conv2_num_filters = 64
-# conv3_num_filters = 128
-# conv4_num_filters = 160
-# conv5_num_filters = 192
-# conv6_num_filters = 192
-# conv7_num_filters = 192
-# conv8_num_filters = 192
-# fc1_num_features = 3072
-# fc2_num_features = 3072
-# fc3_num_features = 3072
-# convolution_kernel_size = 3
-#
-# sess = tf.InteractiveSession()
-#
-# # Initialize input and training label placeholder variables.
-# X = tf.placeholder(tf.float32, shape=[None, image_size, image_size, num_colour_channels])
-# y_length = tf.placeholder(tf.float32, shape=[None, num_length_classes], name='y_length')
-# y_length_cls = tf.argmax(y_length, dimension=1)
-# y_digit1 = tf.placeholder(tf.float32, shape=[None, num_digit_classes], name='y_digit1')
-# y_digit1_cls = tf.argmax(y_digit1, dimension=1)
-# y_digit2 = tf.placeholder(tf.float32, shape=[None, num_digit_classes], name='y_digit2')
-# y_digit2_cls = tf.argmax(y_digit2, dimension=1)
-# y_digit3 = tf.placeholder(tf.float32, shape=[None, num_digit_classes], name='y_digit3')
-# y_digit3_cls = tf.argmax(y_digit3, dimension=1)
-# y_digit4 = tf.placeholder(tf.float32, shape=[None, num_digit_classes], name='y_digit4')
-# y_digit4_cls = tf.argmax(y_digit4, dimension=1)
-# y_digit5 = tf.placeholder(tf.float32, shape=[None, num_digit_classes], name='y_digit5')
-# y_digit5_cls = tf.argmax(y_digit5, dimension=1)
-#
-# # Use the same dropout probability for all applicable layers.
-# dropout_probability = tf.placeholder(tf.float32)
-#
-# # Construct convolutional layers.
-# conv1 = input_conv_layer(X, convolution_kernel_size, num_colour_channels, conv1_num_filters)
-# conv2 = conv_layer(conv1, convolution_kernel_size, conv1_num_filters, conv2_num_filters, dropout_probability)
-# conv3 = conv_layer(conv2, convolution_kernel_size, conv2_num_filters, conv3_num_filters, dropout_probability, pool=True)
-# conv4 = conv_layer(conv3, convolution_kernel_size, conv3_num_filters, conv4_num_filters, dropout_probability)
-# conv5 = conv_layer(conv4, convolution_kernel_size, conv4_num_filters, conv5_num_filters, dropout_probability, pool=True)
-# conv6 = conv_layer(conv5, convolution_kernel_size, conv5_num_filters, conv6_num_filters, dropout_probability)
-# conv7 = conv_layer(conv6, convolution_kernel_size, conv6_num_filters, conv7_num_filters, dropout_probability, pool=True)
-# conv8 = conv_layer(conv7, convolution_kernel_size, conv7_num_filters, conv8_num_filters, dropout_probability)
-#
-# pooled_image_size = image_size / 2**3
-# fc1_input_size = pooled_image_size*pooled_image_size*conv8_num_filters
-#
-# # Logistic classifier for y_length
-# length_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# length_fc2 = dense_layer(length_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# length_final = dense_layer(length_fc2, fc2_num_features, num_length_classes, dropout_probability)
-# # Logistic classifier for digit 1
-# digit1_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# digit1_fc2 = dense_layer(digit1_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# digit1_final = dense_layer(digit1_fc2, fc2_num_features, num_digit_classes, dropout_probability)
-# # Logistic classifier for digit 2
-# digit2_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# digit2_fc2 = dense_layer(digit2_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# digit2_final = dense_layer(digit2_fc2, fc2_num_features, num_digit_classes, dropout_probability)
-# # Logistic classifier for digit 3
-# digit3_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# digit3_fc2 = dense_layer(digit3_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# digit3_final = dense_layer(digit3_fc2, fc2_num_features, num_digit_classes, dropout_probability)
-# # Logistic classifier for digit 4
-# digit4_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# digit4_fc2 = dense_layer(digit4_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# digit4_final = dense_layer(digit4_fc2, fc2_num_features, num_digit_classes, dropout_probability)
-# # Logistic classifier for digit 5
-# digit5_fc1 = dense_layer(conv8, fc1_input_size, fc1_num_features, dropout_probability)
-# digit5_fc2 = dense_layer(digit5_fc1, fc1_num_features, fc2_num_features, dropout_probability)
-# digit5_final = dense_layer(digit5_fc2, fc2_num_features, num_digit_classes, dropout_probability)
-#
-# # Training for y_length
-# cross_entropy_length = tf.nn.softmax_cross_entropy_with_logits(logits=length_final, labels=y_length)
-# length_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_length)
-# correct_length = tf.equal(tf.argmax(length_final, 1), tf.argmax(y_length, 1))
-# length_accuracy = tf.reduce_mean(tf.cast(correct_length, tf.float32))
-# # Training for digit 1
-# cross_entropy_digit1 = tf.nn.softmax_cross_entropy_with_logits(logits=digit1_final, labels=y_digit1)
-# digit1_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_digit1)
-# correct_digit1 = tf.equal(tf.argmax(digit1_final, 1), tf.argmax(y_digit1, 1))
-# # Training for digit 2
-# cross_entropy_digit2 = tf.nn.softmax_cross_entropy_with_logits(logits=digit2_final, labels=y_digit2)
-# digit2_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_digit2)
-# correct_digit2 = tf.equal(tf.argmax(digit2_final, 1), tf.argmax(y_digit2, 1))
-# # Training for digit 3
-# cross_entropy_digit3 = tf.nn.softmax_cross_entropy_with_logits(logits=digit3_final, labels=y_digit3)
-# digit3_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_digit3)
-# correct_digit3 = tf.equal(tf.argmax(digit3_final, 1), tf.argmax(y_digit3, 1))
-# # Training for digit 4
-# cross_entropy_digit4 = tf.nn.softmax_cross_entropy_with_logits(logits=digit4_final, labels=y_digit4)
-# digit4_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_digit4)
-# correct_digit4 = tf.equal(tf.argmax(digit4_final, 1), tf.argmax(y_digit4, 1))
-# # Training for digit 5
-# cross_entropy_digit5 = tf.nn.softmax_cross_entropy_with_logits(logits=digit5_final, labels=y_digit5)
-# digit5_optimizer = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy_digit5)
-# correct_digit5 = tf.equal(tf.argmax(digit5_final, 1), tf.argmax(y_digit5, 1))
-#
-# sess.run(tf.initialize_all_variables())
-# for i in range(2000):
-#     batch = sample_training(normalized_training_set, 50)
-#     if i%100 == 0:
-#         acc_len = length_accuracy.eval(feed_dict={X: batch[0], y_length: batch[1], dropout_probability: 1.0})
-#         print('step %d, training accuracy: %g'%(i, acc_len))
-#     length_optimizer.run(feed_dict={X: batch[0], y_length: batch[1], dropout_probability:0.5})
-
